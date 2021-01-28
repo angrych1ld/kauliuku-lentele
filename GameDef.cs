81e7c14f9422390125d60e6774e1e1cfca073dfe
Andrius Bendoraitis <abendoraitis300@gmail.com>
2021-01-28 21:01:54 +0200

    Minor fixes

--- GameDef.cs
+++ GameDef.cs
@@ -140,10 +140,7 @@ namespace KauliukuLentele
             List<int> possiblePairValues = new List<int>();
             for (int i = 1; i <= 6; i++)
             {
-                for (int j = 1; j <= 6; j++)
-                {
-                    possiblePairValues.Add(i + j);
-                }
+                possiblePairValues.Add(i * 2);
             }
 
             possiblePairValues.Sort();
